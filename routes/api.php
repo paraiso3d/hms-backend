a2584b13798433e15633ebd5ddfad4229322aaf0
paraiso3d <normanparaiso.abm12@gmail.com>
2025-10-23 04:00:30 +0800

    AA

--- routes/api.php
+++ routes/api.php
@@ -51,7 +51,7 @@ Route::get('/getpatients/{id}', [PatientController::class, 'getPatientById']);
 Route::post('/createpatient', [PatientController::class, 'createPatient']);
 Route::post('/updatepatient/{id}', [PatientController::class, 'updatePatient']);
 Route::post('/deletepatient/{id}', [PatientController::class, 'deletePatient']);
-Route::post('getmyappointments', [PatientController::class, 'getMyAppointments'])->middleware('auth:sanctum');
+Route::get('getmyappointments', [PatientController::class, 'getMyAppointments'])->middleware('auth:sanctum');
 
 
 // ---------------------
