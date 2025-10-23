6f8e927fd8de8aac5f4afc4b4aaf537709860f09
paraiso3d <normanparaiso.abm12@gmail.com>
2025-10-24 00:26:50 +0800

    AA

--- app/Http/Controllers/AppointmentController.php
+++ app/Http/Controllers/AppointmentController.php
@@ -118,7 +118,7 @@ class AppointmentController extends Controller
             if (!empty($search)) {
                 $query->where(function ($q) use ($search) {
                     $q->whereHas('patient', function ($sub) use ($search) {
-                        $sub->where('patient_name', 'like', "%{$search}%");
+                        $sub->where('full_name', 'like', "%{$search}%");
                     })
                         ->orWhereHas('doctor', function ($sub) use ($search) {
                             $sub->where('doctor_name', 'like', "%{$search}%");
